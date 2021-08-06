03e62f6e57e0924bea3ac2546dd868e9d759076c
ricopin78 <gtm.aksf.ss2738@gmail.com>
2021-08-06 13:38:13 +0000

    START-titles: add controllers and title-new-view-page

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -1,13 +1,32 @@
 class TitlesController < ApplicationController
+
   def new
+    @title = Title.new
   end
 
   def create
+    @title = Title.new
+    @title.save
+    redirect_to user_path
   end
 
   def update
+    @title = Title.find(params[id])
+    @title.update(title_params)
+    redirect_to user_path
   end
 
   def destroy
+    @title = Title.find(params[id])
+    @title.destroy
+    redirect_to user_path
+
   end
+
+  private
+
+  def title_params
+    params.require(:title).permit(:department, :position)
+  end
+
 end
