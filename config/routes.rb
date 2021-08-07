970c0fcaeb08399d4c53cfc03f4563c843ced1e3
ricopin78 <gtm.aksf.ss2738@gmail.com>
2021-08-07 04:44:18 +0000

    Update routes: add todos routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
   resources :relevant_parties, only:[:create,:update,:destroy]
   resources :attachments, only:[:create,:destroy]
   resources :event_todos, only:[:create,:update,:destroy]
+  resources :todos, only:[:index,:show,:create,:edit,:update,:destroy]
   resources :events
   resources :comments, only:[:index,:create,:edit,:update,:show,:destroy]
   resources :titles, only:[:new,:create,:update,:destroy]
   resources :users, only:[:show]
-  root to: 'home#1top'
+  root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
