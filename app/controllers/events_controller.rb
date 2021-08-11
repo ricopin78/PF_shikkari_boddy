40be642188776d298deab03a4e17a0e3c8752222
ricopin78 <gtm.aksf.ss2738@gmail.com>
2021-08-11 12:35:55 +0000

    Update:database

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,9 +6,14 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new
+    @event.user_id = current_user.id
+    @event.save
+    redirect_to events_path
   end
 
   def edit
