d89bb98b4555c0e019cf41661fe9e6e00394ee98
ricopin78 <gtm.aksf.ss2738@gmail.com>
2021-08-29 07:04:41 +0000

    Update event: add attendance

--- app/controllers/relevant_parties_controller.rb
+++ app/controllers/relevant_parties_controller.rb
@@ -1,10 +1,23 @@
 class RelevantPartiesController < ApplicationController
-  def create
-  end
 
   def update
+    @relevant_party = RelevantParty.find_by(params[:id],params[:event_id])
+    @relevant_party.user_id = current_user.id
+    if @relevant_party.update(relevant_party_params)
+      redirect_to event_path
+    else
+      render "events/show"
+    end
   end
 
-  def destroy
+  private
+
+  def relevant_party_params
+    params.permit(:attendance, :event_id, :user_id)
   end
+
+  def relevant_partu_params
+    params.require(:todo).permit(:title, :body, :user_id, :deadline, :duration, :completed, :priority)
+  end
+
 end
