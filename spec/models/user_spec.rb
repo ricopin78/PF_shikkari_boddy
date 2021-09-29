e8192a658eb19d64a944f148f9ea217e7806cb47
ricopin78 <gtm.aksf.ss2738@gmail.com>
2021-09-29 14:13:10 +0000

    Rspec user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,43 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  let(:user) { build(:user, last_name: nil)}
-
-
   describe '#create' do
+    let(:user) { build(:user, last_name: nil, first_name: nil, last_name_kana: nil, first_name_kana: nil, email: nil, company: nil, password: nil)}
     it "last_nameがない場合は登録できないこと" do
       user.valid?
       expect(user.errors[:last_name]).to include("が入力されていません。")
     end
+
+    it "first_nameがない場合は登録できないこと" do
+      user.valid?
+      expect(user.errors[:first_name]).to include("が入力されていません。")
+    end
+
+    it "last_name_kanaがない場合は登録できないこと" do
+      user.valid?
+      expect(user.errors[:last_name_kana]).to include("が入力されていません。")
+    end
+
+    it "first_name_kanaがない場合は登録できないこと" do
+      user.valid?
+      expect(user.errors[:first_name_kana]).to include("が入力されていません。")
+    end
+
+    it "emailがない場合は登録できないこと" do
+      user.valid?
+      expect(user.errors[:email]).to include("が入力されていません。")
+    end
+
+    it "companyがない場合は登録できないこと" do
+      user.valid?
+      expect(user.errors[:company]).to include("が入力されていません。")
+    end
+
+    it "passwordがない場合は登録できないこと" do
+      user.valid?
+      expect(user.errors[:password]).to include("が入力されていません。")
+    end
+
   end
 
 end
